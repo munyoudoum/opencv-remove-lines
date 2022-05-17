e7204c5d144fefb287c5236289642271deefb924
munyoudoum <munyoudoum@gmail.com>
2022-05-17 11:11:25 +0700

    feat: add working area and ratio

--- remove_straight_line.py
+++ remove_straight_line.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Read in image, grayscale, and Otsu's threshold
-image = cv2.imread('/Users/munyoudoum/Downloads/C10_img.png')
+image = cv2.imread('/Users/munyoudoum/Downloads/C5_img.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 thresh = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
@@ -15,15 +15,20 @@ opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
 # Find contours and filter using contour area to remove noise
 cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+working_area = 0
 for c in cnts:
     area = cv2.contourArea(c)
     if area < 100:
         cv2.drawContours(opening, [c], -1, (0,0,0), -1)
     else:
         cv2.putText(opening, "Area: {}".format(area), (c[0][0][0], c[0][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+        working_area += area
     # else:
     # cv2.drawContours(opening, [c], -1, (0,255,255), -1)
 
+cv2.putText(opening, "Working Area: {}".format(working_area), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+cv2.putText(opening, "Total Area: {}".format(image.shape[0]*image.shape[1]), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+cv2.putText(opening, "Ratio: {}".format(working_area/(image.shape[0]*image.shape[1])*100), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
 # Bitwise-xor with original image
 opening = cv2.merge([opening, opening, opening])
 result = cv2.bitwise_xor(image, opening)
