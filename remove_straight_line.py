8f721e8559bb0bc5e1599032ff5f0fa0a8d09f4b
munyoudoum <munyoudoum@gmail.com>
2022-06-06 00:34:04 +0700

    feat: integrate background removal to contour detection

--- remove_straight_line.py
+++ remove_straight_line.py
@@ -1,13 +1,34 @@
 import cv2
 import numpy as np
 import glob
+from PIL import Image, ImageFilter
 
 DEBUG = 1
 
 for filename in glob.glob('/Users/munyoudoum/Desktop/compute_vision/remove_lines/plots/**/*.png', recursive=True):
     print(filename)
     # Read in image, grayscale, and Otsu's threshold
-    image = cv2.imread(filename)
+    # image = cv2.imread(filename)
+
+    im = Image.open(filename).convert('RGB')
+    na = np.array(im)
+    orig = na.copy()    # Save original
+
+    # Median filter to remove outliers
+    im = im.filter(ImageFilter.MedianFilter(3))
+
+    # Find X,Y coordinates of all yellow pixels
+    yellowY, yellowX = np.where(np.all(na==[229,236,246],axis=2))
+
+    top, bottom = yellowY[0], yellowY[-1]
+    left, right = yellowX[0], yellowX[-1]
+    # print(top,bottom,left,right)
+
+    # Extract Region of Interest from unblurred original
+    orig= cv2.cvtColor(orig, cv2.COLOR_BGR2RGB)
+    image = orig[top:bottom, left:right]
+
+
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
@@ -39,14 +60,14 @@ for filename in glob.glob('/Users/munyoudoum/Desktop/compute_vision/remove_lines
     cv2.putText(opening, "Working Area: {}".format(working_area), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
     cv2.putText(opening, "Total Area: {}".format(image.shape[0]*image.shape[1]), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
     cv2.putText(opening, "Ratio: {}".format(working_area/(image.shape[0]*image.shape[1])*100), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
-    # draw line in 10% of x axis
-    cv2.line(opening, (int(image.shape[1]*0.08), 0), (int(image.shape[1]*0.08), image.shape[0]), (255,255,255), 2)
-    # draw a horizontal line in 11% of y axis
-    cv2.line(opening, (0, int(image.shape[0]*0.11)), (image.shape[1], int(image.shape[0]*0.11)), (255,255,255), 2)
-    # draw a horizontal line in 85% of y axis
-    cv2.line(opening, (0, int(image.shape[0]*0.85)), (image.shape[1], int(image.shape[0]*0.85)), (255,255,255), 2)
-    # draw vertical line in 92% of x axis
-    cv2.line(opening, (int(image.shape[1]*0.92), 0), (int(image.shape[1]*0.92), image.shape[0]), (255,255,255), 2)
+    # # draw line in 10% of x axis
+    # cv2.line(opening, (int(image.shape[1]*0.08), 0), (int(image.shape[1]*0.08), image.shape[0]), (255,255,255), 2)
+    # # draw a horizontal line in 11% of y axis
+    # cv2.line(opening, (0, int(image.shape[0]*0.11)), (image.shape[1], int(image.shape[0]*0.11)), (255,255,255), 2)
+    # # draw a horizontal line in 85% of y axis
+    # cv2.line(opening, (0, int(image.shape[0]*0.85)), (image.shape[1], int(image.shape[0]*0.85)), (255,255,255), 2)
+    # # draw vertical line in 92% of x axis
+    # cv2.line(opening, (int(image.shape[1]*0.92), 0), (int(image.shape[1]*0.92), image.shape[0]), (255,255,255), 2)
 
     # Bitwise-xor with original image
     opening = cv2.merge([opening, opening, opening])
--- remove_white_background.py
+++ remove_white_background.py
@@ -1,6 +1,9 @@
+# https://stackoverflow.com/questions/60780831/python-how-to-cut-out-an-area-with-specific-color-from-image-opencv-numpy
 import glob
 import numpy as np
 from PIL import Image, ImageFilter
+import cv2
+
 
 for filename in glob.glob('/Users/munyoudoum/Desktop/compute_vision/remove_lines/plots/**/*.png', recursive=True):
     # Open image and make into Numpy array
@@ -19,6 +22,10 @@ for filename in glob.glob('/Users/munyoudoum/Desktop/compute_vision/remove_lines
     print(top,bottom,left,right)
 
     # Extract Region of Interest from unblurred original
+    orig= cv2.cvtColor(orig, cv2.COLOR_BGR2RGB)
     ROI = orig[top:bottom, left:right]
+    cv2.imshow("original", orig)
+    cv2.imshow("ROI", ROI)
+    cv2.waitKey(0)
     filename = "_".join(filename.rsplit(".",1)[0].rsplit("/")[-2:])
-    Image.fromarray(ROI).save(f'/Users/munyoudoum/Desktop/compute_vision/remove_lines/cropped/cropped-{filename}.png')
+    # Image.fromarray(ROI).save(f'/Users/munyoudoum/Desktop/compute_vision/remove_lines/cropped/cropped-{filename}.png')
